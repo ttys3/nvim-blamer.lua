db43ae622dbec1ba3fd8172c2d4fed1b2980c39c
荒野無燈 <ttys3@outlook.com>
2021-01-14 02:46:20 +0800

    fix: bypass ft list: rename LuaTree to NvimTree. do not show Not Committed Yet msg

--- lua/blamer.lua
+++ lua/blamer.lua
@@ -3,39 +3,40 @@ local M = {}
 local api = vim.api
 
 -- skip scratch buffer or unkown filetype, nvim's terminal window, and other known filetypes need to bypass
-local bypass_ft = {'', 'bin', '.', 'vim-plug', 'LuaTree', 'startify', 'nerdtree'}
+local bypass_ft = { '', 'bin', '.', 'vim-plug', 'NvimTree', 'startify', 'nerdtree' }
 
 function M.blameVirtText()
-  if vim.bo.buftype ~= '' then
-    return
-  end
-
-  for _,v in ipairs(bypass_ft) do
-    if vim.bo.filetype == v then
-      return
+    if vim.bo.buftype ~= '' then
+        return
     end
-  end
 
-  api.nvim_buf_clear_namespace(0, 2, 0, -1) -- clear out virtual text from namespace 2 (the namespace we will set later)
-  local currFile = vim.fn.expand('%')
-  local line = api.nvim_win_get_cursor(0)
-  local blame = vim.fn.system(string.format('git blame -c -L %d,%d %s', line[1], line[1], currFile))
-  local hash = vim.split(blame, '%s')[1]
-  local cmd = string.format("git show %s ", hash).."--format='🐛 %an | %ar | %s'"
-  if hash == '00000000' then
-    text = 'Not Committed Yet'
-  else
-    text = vim.fn.system(cmd)
-    text = vim.split(text, '\n')[1]
-    if text:find("fatal") then -- if the call to git show fails
-      text = 'Not Committed Yet'
+    for _, v in ipairs(bypass_ft) do
+        if vim.bo.filetype == v then
+            return
+        end
     end
-  end
-  api.nvim_buf_set_virtual_text(0, 2, line[1] - 1, {{ text,'GitLens' }}, {}) -- set virtual text for namespace 2 with the content from git and assign it to the higlight group 'GitLens'
+
+    api.nvim_buf_clear_namespace(0, 2, 0, -1) -- clear out virtual text from namespace 2 (the namespace we will set later)
+    local currFile = vim.fn.expand('%')
+    local line = api.nvim_win_get_cursor(0)
+    local blame = vim.fn.system(string.format('git blame -c -L %d,%d %s', line[1], line[1], currFile))
+    local hash = vim.split(blame, '%s')[1]
+    local cmd = string.format("git show %s ", hash) .. "--format='🐛 %an | %ar | %s'"
+    local text = ''
+    if hash == '00000000' then
+    -- text = 'Not Committed Yet'
+    else
+        text = vim.fn.system(cmd)
+        text = vim.split(text, '\n')[1]
+        if text:find("fatal") then -- if the call to git show fails
+            text = 'err: ' .. text
+        end
+    end
+    api.nvim_buf_set_virtual_text(0, 2, line[1] - 1, { { text, 'GitLens' } }, {}) -- set virtual text for namespace 2 with the content from git and assign it to the higlight group 'GitLens'
 end
 
 function M.clearBlameVirtText() -- important for clearing out the text when our cursor moves
-  api.nvim_buf_clear_namespace(0, 2, 0, -1)
+    api.nvim_buf_clear_namespace(0, 2, 0, -1)
 end
 
 return M
